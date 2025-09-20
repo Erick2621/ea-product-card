cd1e75b0601bd6e6fae5dadbcded68d22e95c9df
Erick2621 <erickalonso2619@hotmail.com>
2025-09-20 17:12:16 -0500

    v1.0.7

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,20 +1,14 @@
 import { defineConfig } from 'tsup'
 
-export default defineConfig(
-    {
-        entry: ['src/index.ts'],
-        splitting: false,
-        sourcemap: true,
-        clean: true,
-        dts: true,
-        format: ['cjs', 'esm'],
-        tsconfig: './tsconfig.app.json', // 👈 aquí la magia
-        loader: {
-            '.jpg': 'file',   // convierte los imports de jpg en URLs con hash
-            '.png': 'file',   // idem para PNG
-            '.svg': 'file',   // idem para SVG
-            '.css': 'css'     // permite importar CSS modules
-        }
-    },
-
-)
+export default defineConfig({
+    entry: ['src/index.ts'],     // Tu punto de entrada principal
+    format: ['cjs', 'esm'],      // Genera CommonJS y ESM
+    dts: true,                   // Genera archivos .d.ts
+    sourcemap: true,             // Opcional, para debug
+    clean: true,                 // Limpia dist antes de compilar
+    splitting: false,            // Para librerías, normalmente false
+    tsconfig: 'tsconfig.app.json',
+    outDir: 'dist',              // Carpeta de salida
+    minify: false,               // Opcional
+    legacyOutput: true,         // Organiza los archivos por formato
+})
